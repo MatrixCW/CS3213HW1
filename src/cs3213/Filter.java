5918cb2bb4f1407c2cd393f8153af787b076658d
Sai <jiangyaoxuan@gmail.com>
2013-08-26 10:49:53 +0800

    modify filter abstract class

--- src/cs3213/Filter.java
+++ src/cs3213/Filter.java
@@ -1,5 +1,21 @@
 package cs3213;
 
-public abstract class Filter {
-
+public abstract class Filter implements Runnable{
+	protected Pipe inputPipe;
+	protected Pipe outputPipe;
+	
+	public Filter(Pipe inputP, Pipe outputP){
+		inputPipe = inputP;
+		outputPipe = outputP;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			perform();
+		}
+	}
+	
+	public abstract void perform();
 }
