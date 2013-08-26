eaa3c33d612bbbe6099fa3cdf0f9a65729f1bdbe
Sai <jiangyaoxuan@gmail.com>
2013-08-26 10:40:28 +0800

    add filter class

--- src/cs3213/Filter.java
+++ src/cs3213/Filter.java
@@ -1,12 +1,5 @@
 package cs3213;
 
-public abstract class Filter extends Runnable {
-	private Pipe input_Pipe;
-	private Pipe output_Pipe;
-	
-	public Filter(Pipe in_Pipe, Pipe out_Pipe){
-		input_Pipe = in_Pipe;
-		output_Pipe = out_Pipe;
-	}
-	
+public abstract class Filter {
+
 }
