a809283c27a12017e95d815e638f5ab4a64379bf
Sai <jiangyaoxuan@gmail.com>
2013-08-26 11:55:41 +0800

    remove unneeded synchronized symbol

--- src/cs3213/Input.java
+++ src/cs3213/Input.java
@@ -3,35 +3,31 @@ package cs3213;
 import java.util.ArrayList;
 
 public class Input extends Filter{
-	private boolean hasInput;
 	private ArrayList<String> outputList;
 	
 	public Input(Pipe inputP, Pipe outputP) {
 		super(inputP, outputP);
-		hasInput = false;
 		outputList = new ArrayList<String>();
 	}
 
 	@Override
-	protected synchronized void perform() {
+	protected void perform() {
 		while(true){
-			if(outputPipe.isReadyToWrite() && hasInput){
+			if(outputPipe.isReadyToWrite() && outputList.size() == 1){
 				outputPipe.write(outputList);
 				outputList.remove(0);
-				hasInput = false;
 				
 				break;
 			}
 		}
 	}
 	
-	public synchronized void inputStream(String str){
-		hasInput = true;
+	public void inputStream(String str){
 		outputList.add(str);
 	}
 	
-	public synchronized boolean isReadyToWriteInto(){
-		return !hasInput;
+	public boolean isReadyToWriteInto(){
+		return (outputList.size() == 0)?true:false;
 	}
 
 }
